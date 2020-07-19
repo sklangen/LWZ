446ff04de1f22c531bc58c7ced71e946f7a6a918
Oshgnacknak <osh@oshgnacknak.de>
2020-07-20 00:22:43 +0200

    Use datetime.date

--- lwz/dewis.py
+++ lwz/dewis.py
@@ -2,15 +2,24 @@ from .SeasonDirectory import SeasonPlayer
 from .utils import *
 from concurrent.futures import ThreadPoolExecutor 
 from csv import DictReader
-from datetime import datetime
 from io import StringIO
 from typing import Iterable
+import datetime
 import lxml.html as lh
 import math
 
 
 class DewisException(Exception):
     pass
+
+
+def _row_to_player(row):
+    return SeasonPlayer(
+        id=int(row['id']),
+        dwz=int_or_default(row['dwz'], 0),
+        firstname=row['vorname'],
+        lastname=row['nachname'],
+    )
 
 
 def get_club(zps: str) -> Iterable[SeasonPlayer]:
@@ -22,12 +31,18 @@ def get_club(zps: str) -> Iterable[SeasonPlayer]:
 
     f = StringIO(content)
     for row in DictReader(f, delimiter='|'):
-        yield SeasonPlayer(
-            id=int(row['id']),
-            dwz=int_or_default(row['dwz'], 0),
-            firstname=row['vorname'],
-            lastname=row['nachname'],
-        )
+        yield _row_to_player(row)
+
+
+def get_player(pkz: int) -> SeasonPlayer:
+    url = f'https://www.schachbund.de/php/dewis/spieler.php?pkz={pkz}&format=csv'
+    content = http_get(url)
+
+    if content.startswith('Fehler: '):
+        raise DewisException(content)
+
+    for row in DictReader(content.split('\n')[:2], delimiter='|'):
+        return _row_to_player(row)
 
 
 def get_participations(pkz: int) -> DictReader:
@@ -41,9 +56,9 @@ def get_participations(pkz: int) -> DictReader:
     return DictReader(lines[index:], delimiter='|')
 
 
-def extract_date_from_tournament(tournament_id: str) -> datetime:
+def extract_date_from_tournament(tournament_id: str) -> datetime.date:
     if tournament_id == 'B053-000-DWZ':
-        return datetime(2009, 1, 1)
+        return datetime.date(2009, 1, 1)
 
     url = f'https://www.schachbund.de/turnier/{tournament_id}.html'
     doc = lh.fromstring(http_get(url))
@@ -60,17 +75,20 @@ def extract_date_from_tournament(tournament_id: str) -> datetime:
                 continue
             if correct_row:
                 date_string = el.text_content().split(' ')[0]
-                return datetime.strptime(date_string, '%d.%m.%Y')
+                return datetime.datetime.strptime(date_string, '%d.%m.%Y').date()
 
 
-def get_player_rating_at(pkz: int, ref_date: datetime) -> int:
+def get_player_rating_at(pkz: int, ref_date: datetime.date) -> int:
     participations = list(get_participations(pkz))
+
+    if not participations:
+        return 0
 
     with ThreadPoolExecutor(max_workers=4) as e:
         dates = e.map(lambda p: extract_date_from_tournament(p['turniercode']), participations)
 
     for participation, date in zip(participations, dates):
         if date > ref_date:
-            return int_or_default(participation['dwzalt'], default=0)
+            return int_or_default(participation['dwzalt'], 0)
 
-    return int_or_default(participations[-1]['dwzneu'], default=0)
+    return int_or_default(participations[-1]['dwzneu'], 0)
--- tests/test_dewis.py
+++ tests/test_dewis.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 from lwz.dewis import *
 import unittest
 
@@ -6,13 +6,19 @@ import unittest
 class TestPastRatings(unittest.TestCase):
 
     def test_get_player_rating_at(self):
-        ref_date = datetime(2018, 5, 1)
+        ref_date = date(2018, 5, 1)
         r = get_player_rating_at(10562743, ref_date)
         self.assertIsInstance(r, int)
         self.assertEqual(r, 1684)
 
     def test_that_an_future_date_has_none_0_rating(self):
-        ref_date = datetime(3018, 2, 15)
+        ref_date = date(3018, 2, 15)
         r = get_player_rating_at(10029745, ref_date)
         self.assertIsInstance(r, int)
         self.assertNotEqual(r, 0)
+
+    def test_no_tournaments_has_0_rating(self):
+        ref_date = date(3018, 2, 15)
+        r = get_player_rating_at(10583181, ref_date)
+        self.assertIsInstance(r, int)
+        self.assertEqual(r, 0)

e9eaf8fe128458409b029285137d99a2cfa28e2f
Oshgnacknak <osh@oshgnacknak.de>
2021-04-09 23:18:18 +0200

    Add rank column to generated html

--- lwz/render.py
+++ lwz/render.py
@@ -22,7 +22,7 @@ class SeasonDirectoryRenderer:
     def __init__(self, seasonDir: SeasonDirectory):
         self.seasonDir = seasonDir
         self.mode = modes[seasonDir.season.mode]
-        self.headers = ['Name', 'DWZ', 'Attr']
+        self.headers = ['Platz', 'Name', 'DWZ', 'Attr']
 
     @property
     def index(self):
@@ -48,10 +48,11 @@ class SeasonDirectoryRenderer:
             )
 
     def tournament_ranking(self, tournament):
-        for player in sorted(tournament.players, key=lambda p: p.rank):
+        rows = sorted(tournament.players, key=lambda p: p.rank)
+        for i, player in enumerate(rows):
             try:
                 sp = next(filter(lambda p: p.id == player.id, self.seasonDir.all_players))
-                yield sp.name, sp.dwz, self.mode.get_attr(sp), player.points
+                yield [i+1, sp.name, sp.dwz, self.mode.get_attr(sp), player.points]
             except StopIteration:
                 logging.exception('No SeasonPlayer for: ' + str(player))
                 raise
@@ -64,11 +65,12 @@ class SeasonDirectoryRenderer:
 
     @property
     def season_rows(self):
-        for player, score, months in sorted(self.season_rows_calculated, key=lambda t: t[1], reverse=True):
-            yield [player.name, player.dwz, self.mode.get_attr(player), self.mode.format_score(score)] + months
+        season_results = sorted(self.season_results_calculated, key=lambda t: t[1], reverse=True)
+        for i, (player, score, months) in enumerate(season_results):
+            yield [i+1, player.name, player.dwz, self.mode.get_attr(player), self.mode.format_score(score)] + months
 
     @property
-    def season_rows_calculated(self):
+    def season_results_calculated(self):
         for player in self.seasonDir.all_players:
             months_played = dict(self.seasonDir.months_played(player))
 
